7efd8a403a841e1a443c8415816d6f55c47e1344
alexthwining <14134483+alexthwining@users.noreply.github.com>
2022-08-08 22:13:12 -0700

    simplified code to loop seamlessly

--- rps.js
+++ rps.js
@@ -5,6 +5,17 @@ const SCISSORS = "scissors";
 let pScore = 0; // keeps tracks of player score
 let cScore = 0; // track computer score
 
+// custom divs
+const btns = document.querySelector("#btns");
+const score = document.createElement("div");
+score.setAttribute("id", "score");
+const res = document.createElement("div");
+res.setAttribute("id", "res");
+
+btns.appendChild(score);
+btns.appendChild(res);
+
+
 function computerPlay() {
     let rps = Math.floor(Math.random() * 3);
     switch (rps) {
@@ -52,54 +63,6 @@ function playRound(playerSelection, computerSelection) {
     }    
 }
 
-// lets the player play by typing out the selection, will continually
-// prompt the player until they enter a valid input
-function orig_game() {
-    let playerSelection = prompt("Pick rock, paper, or scissors!");
-
-    // checking for correct player input. prompt again if not valid
-    while(true) {
-        // make playerSelection all lowercase to allow for easier validity
-        playerSelection = playerSelection.toLowerCase();
-        if(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
-            break;
-        }
-        else {
-            prompt("Enter a valid string: rock, paper, or scissors.");
-        }
-    }
-
-    let pScore = 0, cScore = 0;
-    for(let i=0; i<5; i++) {
-        let computerSelection = computerPlay();
-        let round = playRound(playerSelection, computerSelection);
-        console.log(round); 
-        if(round.includes("win")) {
-            pScore++;
-        }
-        if (round.includes("lose")) {
-            cScore++;
-        }
-    }
-
-    if(pScore > cScore) {
-        let final = `You won the best of 5. ${pScore}-${cScore}!`;
-        console.log(final);
-        alert(final);
-    }
-    else if(pScore == cScore) {
-        let final = `You somehow tied! ${pScore}-${cScore}`;
-        console.log(final);
-        alert(final);
-    }
-    else {
-        let final = `You lost the best of 5. ${pScore}-${cScore}!`;
-        console.log(final);
-        alert(final);
-    }
-
-}
-
 // player plays by clicking buttons as their selection
 function click_game() {
     // button logic for rock button
@@ -127,41 +90,27 @@ function click_game() {
 
 // adds divs to display the score
 function displayScore(cond) {
-    const btns = document.querySelector("#btns");
-    const score = document.createElement("div");
-    score.setAttribute("id", "score");
-
+    if(!document.getElementById("res")){
+        btns.appendChild(score);
+        btns.appendChild(res);
+    }
     if(cond.includes("win")) {
         pScore++;
-        const pwin = document.createElement("div");
-        pwin.setAttribute("id", "pwin");
-        pwin.textContent = "Player wins!";
-        btns.appendChild(pwin);
+        res.textContent = "Player wins!";
     }
     if(cond.includes("lose")) {
         cScore++;
-        const cwin = document.createElement("div");
-        cwin.setAttribute("id", "cwin");
-        cwin.textContent = "Computer win!";
-        btns.appendChild(cwin);
+        res.textContent = "Computer win!";
     }
     if(cond.includes("Draw")) {
-        const draw = document.createElement("div");
-        draw.setAttribute("id", "draw");
-        draw.textContent = "There was a draw!";
-        btns.appendChild(draw);
+        res.textContent = "There was a draw!";
     }
     
     score.textContent = `Player: ${pScore} - Computer ${cScore}`;
-    btns.appendChild(score);
 
     if(pScore == 5 || cScore == 5) {
         winMessage(btns);
-        // disable the 3 buttons so player can't play anymore
-        // const buttons = document.querySelectorAll('button');
-        // buttons.forEach((button) => {
-        //     button.disabled = true;
-        // });
+        // disable all 3 buttons
         document.querySelector("#rbtn").disabled = true;
         document.querySelector("#pbtn").disabled = true;
         document.querySelector("#sbtn").disabled = true;
@@ -172,6 +121,7 @@ function displayScore(cond) {
 function winMessage(btns) {
     const winner = document.createElement("div");
     winner.setAttribute("id", "winner");
+    btns.appendChild(winner);
     if(pScore == 5) {
         winner.textContent = "Congratulations Player, you won the best of 5!";
     }
@@ -179,7 +129,6 @@ function winMessage(btns) {
     if(cScore == 5) {
         winner.textContent = "Boo hoo, you suck. The computer is just better than you.";
     }
-    btns.appendChild(winner);
 
     const ask = document.createElement("div");
     ask.setAttribute("id", "ask");
@@ -204,20 +153,15 @@ function reset() {
         pScore = 0;
         cScore = 0;
         
-        while(!null) {
-            btns.removeChild(pwin);
-            btns.removeChild(cwin);
-            btns.removeChild(score);
-            btns.removeChild(winner);
-            btns.removeChild(ask);
-            btns.removeChild(again);
-        }
+        btns.removeChild(score);
+        btns.removeChild(res);
+        btns.removeChild(winner);
+        btns.removeChild(ask);
+        btns.removeChild(again);
     });
 }
 
 // run the button input game
 click_game();
 
-// run the original game (type out selection);
-//orig_game();
 
